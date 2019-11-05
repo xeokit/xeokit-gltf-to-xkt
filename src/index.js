f6f02db7d722776f3a4ae0c8ca08e476b3f301bb
Amoki <hugo.duroux@gmail.com>
2019-11-05 16:50:23 +0100

    Do not ignore package-lock and keep compatibility with old API

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 const glTFToModel = require('./glTFToModel');
 const modelToXKT = require('./modelToXKT');
 
-module.exports = function glTFToXKT(gltfData) {
+module.exports = {
+  glTFToXKT(gltfData) {
     const gltf = JSON.parse(gltfData);
     const model = glTFToModel(gltf);
     const arrayBuffer = modelToXKT(model);
     return new Buffer.from(arrayBuffer);
-};
+  }
+}
