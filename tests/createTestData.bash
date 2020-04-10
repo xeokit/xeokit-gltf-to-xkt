88678c6cb605cd6f8d1101b574bc592311070ed6
xeolabs <lindsay.kay@xeolabs.com>
2020-04-10 22:05:27 +0200

    Added tests for various XKT versions (1, 3, 4 and 5)

---
+++ tests/createTestData.bash
@@ -0,0 +1,31 @@
+node ../gltf2xkt -f 1 -s models/gltf/WestRiverSideHospital/electrical.gltf -o models/xkt/v1_WestRiverSideHospital_electrical.xkt
+node ../gltf2xkt -f 1 -s models/gltf/WestRiverSideHospital/fireAlarms.gltf -o models/xkt/v1_WestRiverSideHospital_fireAlarms.xkt
+node ../gltf2xkt -f 1 -s models/gltf/WestRiverSideHospital/plumbing.gltf -o models/xkt/v1_WestRiverSideHospital_plumbing.xkt
+node ../gltf2xkt -f 1 -s models/gltf/WestRiverSideHospital/sprinklers.gltf -o models/xkt/v1_WestRiverSideHospital_sprinklers.xkt
+node ../gltf2xkt -f 1 -s models/gltf/WestRiverSideHospital/structure.gltf -o models/xkt/v1_WestRiverSideHospital_structure.xkt
+node ../gltf2xkt -f 1 -s models/gltf/WestRiverSideHospital/architecture.gltf -o models/xkt/v1_WestRiverSideHospital_architecture.xkt
+node ../gltf2xkt -f 1 -s models/gltf/WestRiverSideHospital/mechanical.gltf -o models/xkt/v1_WestRiverSideHospital_mechanical.xkt
+
+node ../gltf2xkt -f 3 -s models/gltf/WestRiverSideHospital/electrical.gltf -o models/xkt/v3_WestRiverSideHospital_electrical.xkt
+node ../gltf2xkt -f 3 -s models/gltf/WestRiverSideHospital/fireAlarms.gltf -o models/xkt/v3_WestRiverSideHospital_fireAlarms.xkt
+node ../gltf2xkt -f 3 -s models/gltf/WestRiverSideHospital/plumbing.gltf -o models/xkt/v3_WestRiverSideHospital_plumbing.xkt
+node ../gltf2xkt -f 3 -s models/gltf/WestRiverSideHospital/sprinklers.gltf -o models/xkt/v3_WestRiverSideHospital_sprinklers.xkt
+node ../gltf2xkt -f 3 -s models/gltf/WestRiverSideHospital/structure.gltf -o models/xkt/v3_WestRiverSideHospital_structure.xkt
+node ../gltf2xkt -f 3 -s models/gltf/WestRiverSideHospital/architecture.gltf -o models/xkt/v3_WestRiverSideHospital_architecture.xkt
+node ../gltf2xkt -f 3 -s models/gltf/WestRiverSideHospital/mechanical.gltf -o models/xkt/v3_WestRiverSideHospital_mechanical.xkt
+
+node ../gltf2xkt -f 4 -s models/gltf/WestRiverSideHospital/electrical.gltf -o models/xkt/v4_WestRiverSideHospital_electrical.xkt
+node ../gltf2xkt -f 4 -s models/gltf/WestRiverSideHospital/fireAlarms.gltf -o models/xkt/v4_WestRiverSideHospital_fireAlarms.xkt
+node ../gltf2xkt -f 4 -s models/gltf/WestRiverSideHospital/plumbing.gltf -o models/xkt/v4_WestRiverSideHospital_plumbing.xkt
+node ../gltf2xkt -f 4 -s models/gltf/WestRiverSideHospital/sprinklers.gltf -o models/xkt/v4_WestRiverSideHospital_sprinklers.xkt
+node ../gltf2xkt -f 4 -s models/gltf/WestRiverSideHospital/structure.gltf -o models/xkt/v4_WestRiverSideHospital_structure.xkt
+node ../gltf2xkt -f 4 -s models/gltf/WestRiverSideHospital/architecture.gltf -o models/xkt/v4_WestRiverSideHospital_architecture.xkt
+node ../gltf2xkt -f 4 -s models/gltf/WestRiverSideHospital/mechanical.gltf -o models/xkt/v4_WestRiverSideHospital_mechanical.xkt
+
+node ../gltf2xkt -f 5 -s models/gltf/WestRiverSideHospital/electrical.gltf -o models/xkt/v5_WestRiverSideHospital_electrical.xkt
+node ../gltf2xkt -f 5 -s models/gltf/WestRiverSideHospital/fireAlarms.gltf -o models/xkt/v5_WestRiverSideHospital_fireAlarms.xkt
+node ../gltf2xkt -f 5 -s models/gltf/WestRiverSideHospital/plumbing.gltf -o models/xkt/v5_WestRiverSideHospital_plumbing.xkt
+node ../gltf2xkt -f 5 -s models/gltf/WestRiverSideHospital/sprinklers.gltf -o models/xkt/v5_WestRiverSideHospital_sprinklers.xkt
+node ../gltf2xkt -f 5 -s models/gltf/WestRiverSideHospital/structure.gltf -o models/xkt/v5_WestRiverSideHospital_structure.xkt
+node ../gltf2xkt -f 5 -s models/gltf/WestRiverSideHospital/architecture.gltf -o models/xkt/v5_WestRiverSideHospital_architecture.xkt
+node ../gltf2xkt -f 5 -s models/gltf/WestRiverSideHospital/mechanical.gltf -o models/xkt/v5_WestRiverSideHospital_mechanical.xkt
---
+++ tests/index.js
@@ -0,0 +1,5 @@
+import {Viewer} from "@xeokit/xeokit-sdk/src/viewer/Viewer.js";
+import {XKTLoaderPlugin} from "@xeokit/xeokit-sdk/src/plugins/XKTLoaderPlugin/XKTLoaderPlugin.js";
+
+export {Viewer};
+export {XKTLoaderPlugin};
