8d865c22dc887486ac04a6057f07b6f67a531d3c
xeolabs <lindsay.kay@xeolabs.com>
2020-04-14 11:18:33 +0200

    Added more test examples

--- tests/createTestData.bash
+++ tests/createTestData.bash
@@ -6,6 +6,11 @@ node ../gltf2xkt -f 1 -s models/gltf/WestRiverSideHospital/structure.gltf -o mod
 node ../gltf2xkt -f 1 -s models/gltf/WestRiverSideHospital/architecture.gltf -o models/xkt/v1_WestRiverSideHospital_architecture.xkt
 node ../gltf2xkt -f 1 -s models/gltf/WestRiverSideHospital/mechanical.gltf -o models/xkt/v1_WestRiverSideHospital_mechanical.xkt
 
+
+# V2
+
+node ../gltf2xkt -f 3 -s models/gltf/schependomlaan/schependomlaan.gltf -o models/xkt/v3_schependomlaan.xkt
+
 node ../gltf2xkt -f 3 -s models/gltf/WestRiverSideHospital/electrical.gltf -o models/xkt/v3_WestRiverSideHospital_electrical.xkt
 node ../gltf2xkt -f 3 -s models/gltf/WestRiverSideHospital/fireAlarms.gltf -o models/xkt/v3_WestRiverSideHospital_fireAlarms.xkt
 node ../gltf2xkt -f 3 -s models/gltf/WestRiverSideHospital/plumbing.gltf -o models/xkt/v3_WestRiverSideHospital_plumbing.xkt
@@ -14,6 +19,10 @@ node ../gltf2xkt -f 3 -s models/gltf/WestRiverSideHospital/structure.gltf -o mod
 node ../gltf2xkt -f 3 -s models/gltf/WestRiverSideHospital/architecture.gltf -o models/xkt/v3_WestRiverSideHospital_architecture.xkt
 node ../gltf2xkt -f 3 -s models/gltf/WestRiverSideHospital/mechanical.gltf -o models/xkt/v3_WestRiverSideHospital_mechanical.xkt
 
+# V3
+
+node ../gltf2xkt -f 4 -s models/gltf/schependomlaan/schependomlaan.gltf -o models/xkt/v4_schependomlaan.xkt
+
 node ../gltf2xkt -f 4 -s models/gltf/WestRiverSideHospital/electrical.gltf -o models/xkt/v4_WestRiverSideHospital_electrical.xkt
 node ../gltf2xkt -f 4 -s models/gltf/WestRiverSideHospital/fireAlarms.gltf -o models/xkt/v4_WestRiverSideHospital_fireAlarms.xkt
 node ../gltf2xkt -f 4 -s models/gltf/WestRiverSideHospital/plumbing.gltf -o models/xkt/v4_WestRiverSideHospital_plumbing.xkt
@@ -21,6 +30,10 @@ node ../gltf2xkt -f 4 -s models/gltf/WestRiverSideHospital/sprinklers.gltf -o mo
 node ../gltf2xkt -f 4 -s models/gltf/WestRiverSideHospital/structure.gltf -o models/xkt/v4_WestRiverSideHospital_structure.xkt
 node ../gltf2xkt -f 4 -s models/gltf/WestRiverSideHospital/architecture.gltf -o models/xkt/v4_WestRiverSideHospital_architecture.xkt
 node ../gltf2xkt -f 4 -s models/gltf/WestRiverSideHospital/mechanical.gltf -o models/xkt/v4_WestRiverSideHospital_mechanical.xkt
+
+# V4
+
+node ../gltf2xkt -f 5 -s models/gltf/schependomlaan/schependomlaan.gltf -o models/xkt/v5_schependomlaan.xkt
 
 node ../gltf2xkt -f 5 -s models/gltf/WestRiverSideHospital/electrical.gltf -o models/xkt/v5_WestRiverSideHospital_electrical.xkt
 node ../gltf2xkt -f 5 -s models/gltf/WestRiverSideHospital/fireAlarms.gltf -o models/xkt/v5_WestRiverSideHospital_fireAlarms.xkt
