802e7a3e774ce1ad6665b0adb51032d120dbc579
xeolabs <lindsay.kay@xeolabs.com>
2020-10-07 19:33:20 +0200

    Decruft

--- tests/index.js
+++
@@ -1,5 +0,0 @@
-import {Viewer} from "@xeokit/xeokit-sdk/src/viewer/Viewer.js";
-import {XKTLoaderPlugin} from "@xeokit/xeokit-sdk/src/plugins/XKTLoaderPlugin/XKTLoaderPlugin.js";
-
-export {Viewer};
-export {XKTLoaderPlugin};
