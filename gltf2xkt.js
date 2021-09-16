8ee7c9298d4ac559d275ae9062d8c673b440c61d
xeolabs <lindsay.kay@xeolabs.com>
2021-09-16 15:08:53 +0200

    Added logging

--- gltf2xkt.js
+++ gltf2xkt.js
@@ -9,7 +9,7 @@ const {
     parseGLTFIntoXKTModel,
     writeXKTModelToArrayBuffer,
     parseMetaModelIntoXKTModel
-} = require("@xeokit/xeokit-xkt-utils/dist/xeokit-xkt-utils.cjs.js");
+} = require("@xeokit/xeokit-convert/dist/xeokit-convert.cjs.js");
 
 const program = new commander.Command();
 
@@ -18,7 +18,8 @@ program.version(package.version, '-v, --version');
 program
     .option('-s, --source [file]', 'path to source glTF file')
     .option('-m, --metamodel [file]', 'path to source metamodel JSON file (optional)')
-    .option('-o, --output [file]', 'path to target xkt file');
+    .option('-o, --output [file]', 'path to target xkt file')
+    .option('-l, --log', 'enable logging');
 
 program.on('--help', () => {
 
@@ -38,33 +39,67 @@ if (program.output === undefined) {
     process.exit(1);
 }
 
-console.log('\n\nReading glTF file: ' + program.source);
+function log(msg) {
+    if (program.log) {
+        console.log("[gltf2xkt] " + msg);
+    }
+}
 
-console.log('Converting to XKT format v8');
+log('Reading glTF file: ' + program.source);
+
+log('Converting to XKT');
 
 async function main() {
+
+    const startTime = new Date();
+
     const gltfBasePath = getBasePath(program.source);
+
     async function getAttachment(name, parsingContext) {
         return fs.readFile(gltfBasePath + name);
     }
+
     const gltfContent = await fs.readFile(program.source);
     let metaModelContent;
     if (program.metamodel) {
         const metaModelData = await fs.readFile(program.metamodel);
         metaModelContent = JSON.parse(metaModelData);
     }
-    const xktContent = await convert(gltfContent, metaModelContent, getAttachment);
-    await fs.writeFile(program.output, xktContent);
+    const stats = {};
+    const xktArraybuffer = await convert(gltfContent, metaModelContent, getAttachment, stats);
+    await fs.writeFile(program.output, xktArraybuffer);
+
+    if (program.log) {
+        const sourceFileSizeBytes = gltfContent.byteLength;
+        const targetFileSizeBytes = xktArraybuffer.byteLength;
+        stats.sourceSize = (sourceFileSizeBytes / 1000).toFixed(2) + " kB";
+        stats.xktSize = (targetFileSizeBytes / 1000).toFixed(2) + " kB";
+        stats.compressionRatio = (sourceFileSizeBytes / targetFileSizeBytes).toFixed(2);
+        stats.conversionTime = ((new Date() - startTime) / 1000.0).toFixed(2) + " secs";
+        for (let key in stats) {
+            const value = stats[key];
+            if (value !== undefined && value !== null && value !== "") {
+                log(key + ": " + value);
+            }
+        }
+    }
 }
 
-async function convert(gltfContent, metaModelContent, getAttachment) {
+async function convert(gltfContent, metaModelContent, getAttachment, stats) {
     const xktModel = new XKTModel();
     if (metaModelContent) {
         await parseMetaModelIntoXKTModel(metaModelContent, xktModel);
     }
     const gltf = JSON.parse(gltfContent);
-    await parseGLTFIntoXKTModel(gltf, xktModel, getAttachment);
+    await parseGLTFIntoXKTModel({
+        data: gltf,
+        xktModel,
+        getAttachment,
+        stats,
+        log
+    });
     xktModel.finalize();
+    stats.aabb = "[" + xktModel.aabb + "]";
     const xktArrayBuffer = writeXKTModelToArrayBuffer(xktModel);
     return Buffer.from(xktArrayBuffer);
 }
